57c28484ca2d1a2c2ed988252faa018b4a3e328c
Gyan Prakash <gyansir22@gmail.com>
2025-04-27 22:46:48 +0530

    deleting_at_any_index.c
    Features:
    Creation of a simple linked list with four nodes.
    
    A function deleteIndex() that deletes the node present at the given index.
    
    A print() function to display the linked list before and after deletion.
    
    Proper memory deallocation of the deleted node using free() to prevent memory leaks.
    
    How It Works:
    The deleteIndex() function traverses the list to reach the node just before the target index.
    
    It adjusts the pointers to skip the node to be deleted.
    
    Then it frees the memory occupied by the removed node.

---
+++ linked_list/singly_linked_list/deleting_at_any_index.c
@@ -0,0 +1,70 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct node
+{
+    int data ;
+    struct node* next ;
+
+};
+
+// deleting the element at a given index 
+
+struct node* deleteIndex(struct node*head , int index ){
+    
+    int i ;
+
+    struct node* p = head ;
+    struct node* q = head-> next;
+    
+    for ( i = 0 ; i < index-1 ; i ++ ){
+        p = p-> next ;
+        q = q-> next ;
+    }
+
+    p -> next = q -> next ;
+
+    free(q);
+      
+    return head ; 
+   
+    
+}
+void print(struct node* head){
+    while(head!=NULL){
+        printf("%d\t",head->data);
+        head = head->next;
+    }
+  printf("\n");
+}
+
+int main(){
+    struct node *head;
+    struct node *second;
+    struct node *third;
+    struct node *forth;
+    
+    head = (struct node*)malloc(sizeof(struct node));
+    second = (struct node*)malloc(sizeof(struct node));
+    third =  (struct node*)malloc(sizeof(struct node));
+    forth = (struct node*)malloc(sizeof(struct node));
+
+    head ->data = 3 ;
+    head-> next = second;
+    second -> data = 4;
+    second->next = third;
+    third -> data = 5;
+    third->next = forth;
+    forth -> data = 6;
+    forth->next = NULL;
+    
+    printf(" linked list before deletion \n");
+    print(head);
+    
+    head = deleteIndex(head , 1 );
+    printf("lined list after deletin \n");
+    
+    print(head);
+    
+    return 0 ;
+}
